5d29934483edff01f69b51f42ab926dfd1797a81
cocahuana <ezequieldominguez11@gmail.com>
2024-05-22 19:21:14 -0300

    Fix: update npm and some config files

--- frontend/react-app-env.d.ts
+++ frontend/react-app-env.d.ts
@@ -1,3 +1,6 @@
-/// <reference types="react-scripts" />
+
+declare module 'react-scripts';
 declare module 'react-dom/client';
 declare module 'react';
+declare module 'node';
+declare module 'process';
